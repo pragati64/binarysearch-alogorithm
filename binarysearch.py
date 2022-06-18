1eaebc1ab1cdabbdd6a964d3f2f2120c173b4ea6
pragati64 <92787955+pragati64@users.noreply.github.com>
2022-06-18 12:37:54 +0530

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,409 @@
+
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+def binarysearch(arr, low, high, z):
+
+    if high >= low:
+
+        mid = (high + low) // 2
+
+
+        if arr[mid] == z:
+            return mid
+
+
+        elif arr[mid] > z:
+            return binarysearch(arr, low, mid - 1, z)
+
+
+        else:
+            return binarysearch(arr, mid + 1, high, z)
+
+    else:
+
+        return -1
+
+
+
+arr = [5, 6, 8, 20, 50]
+z = 20
+
+
+result = binarysearch(arr, 0, len(arr) - 1, z)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
